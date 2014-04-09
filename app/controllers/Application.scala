773c8f530fa7c84bd343b7aaf730a1b30cc68ba6
Owain Lewis - Travel News 1 <owain@owainlewis.com>
2014-04-09 13:20:17 +0100

    first

---
+++ app/controllers/Application.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import play.api.mvc._
+import play.api.libs.iteratee.{Concurrent, Iteratee}
+import play.api.libs.concurrent.Execution.Implicits._
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index(""))
+  }
+
+  def socket = WebSocket.using[String] { request =>
+    val (out,channel) = Concurrent.broadcast[String]
+    val in = Iteratee.foreach[String] {
+      msg => channel.push(msg)
+    }
+
+    (in,out)
+  }
+}
