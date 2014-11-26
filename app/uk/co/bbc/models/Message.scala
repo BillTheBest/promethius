dbe333c8bd315c733bd7d2749b0af1c28df6025e
Owain Lewis <owain@owainlewis.com>
2014-11-26 20:49:21 +0000

    Update Message.scala

--- app/uk/co/bbc/models/Message.scala
+++ app/uk/co/bbc/models/Message.scala
@@ -3,10 +3,10 @@ package uk.co.bbc.models
 import play.api.libs.json.Json
 
 case class Message(
-  key: String,
-  value: String)
+  key   : String,
+  value : String)
 
 object Message {
 
-  implicit val messageFmt = Json.format[Message]
+  implicit val messageFormat = Json.format[Message]
 }
